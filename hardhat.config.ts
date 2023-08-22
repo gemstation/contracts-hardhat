d9926de91cac834cfae297891a67619147961b71
Ramesh Nair <ram@hiddentao.com>
2023-08-22 18:31:28 +0100

    feat: gemforge working!

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.21",
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: process.env.SEPOLIA_RPC_URL,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
